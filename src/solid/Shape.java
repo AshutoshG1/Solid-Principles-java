16fe057692ef100084bbf86c41454cce7f8c3c27
AshutoshG1 <ashutoshgautamspn@gmail.com>
2024-09-30 18:13:37 +0530

    committed all

---
+++ src/solid/Shape.java
@@ -0,0 +1,5 @@
+package solid;
+
+interface Shape {
+    void draw();
+}
---
+++ src/solid/ShapeFactory.java
@@ -0,0 +1,16 @@
+package solid;
+
+class ShapeFactory {
+    // Factory method to create different shapes based on input
+    public Shape createShape(String shapeType) {
+        if (shapeType == null) {
+            return null;
+        }
+        if (shapeType.equalsIgnoreCase("CIRCLE")) {
+            return new Circle();
+        } else if (shapeType.equalsIgnoreCase("SQUARE")) {
+            return new Square();
+        }
+        return null;
+    }
+}
